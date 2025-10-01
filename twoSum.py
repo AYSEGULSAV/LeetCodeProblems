95970fc8d6e38d95f83af61d5a0af588a6c8a6c8
aysefat9 <aysefat9@email.com>
2025-10-01 14:34:52 +0300

    Add Two Sum solution

---
+++ twoSum.py
@@ -0,0 +1,54 @@
+"""Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]"""
+
+from pyclbr import Class
+
+
+class Solutions():
+    def twoSum(self,nums,target):
+        num_to_index={}
+        for i,num in enumerate(nums):
+            complement=target-num
+            if complement in num_to_index:
+                return [num_to_index[complement],i] 
+            num_to_index[num]=i
+
+
+
+solution = Solutions()
+print(solution.twoSum([2,7,11,15], 9))  
+print(solution.twoSum([3,2,4], 6))     
+print(solution.twoSum([3,3], 6))  
+
+print("********************************************")
+#2.yol
+class SolutionSecond():
+    def twoSum(self,nums,target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+               if target==nums[i]+nums[j]:
+                   return[i,j]
+               
+solution=SolutionSecond()
+print(solution.twoSum([2,7,11,15],9))
+print(solution.twoSum([3,2,4],6))
+print(solution.twoSum([3,3],6))
+        
+
+        
+
+              
+
+    
+ 
